df9ca9638d33ba6a63918bdb048cd0a898f33a36
Dieter De Meyer <dieterdemeyer@gmail.com>
2018-07-28 15:59:34 +0200

    Cask for NirvanaHQ

---
+++ Casks/nirvana.rb
@@ -0,0 +1,12 @@
+cask 'resilio-sync-2.6' do
+  version '1.0.0'
+  sha256 'ff860dd0ca7cab4021b22b507fb3f3a8057a64699064cac66d39805bbdc6e5aa'
+
+  url "https://dl.dropboxusercontent.com/s/tspl7jipr5nudpq/Nirvana-#{version}.zip"
+  name 'Nirvana'
+  homepage 'https://nirvanahq.com/'
+
+  auto_updates true
+
+  app 'Nirvana.app'
+end
