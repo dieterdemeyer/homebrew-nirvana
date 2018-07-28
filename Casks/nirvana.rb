04c4962720b313a1b6ec02376d38dac209e65e11
Dieter De Meyer <dieterdemeyer@gmail.com>
2018-07-28 17:55:24 +0200

    Change version

--- Casks/nirvana.rb
+++ Casks/nirvana.rb
@@ -1,5 +1,5 @@
 cask 'nirvana' do
-  version '1.0.0'
+  version '1.0'
   sha256 'ff860dd0ca7cab4021b22b507fb3f3a8057a64699064cac66d39805bbdc6e5aa'
 
   url "https://dl.dropboxusercontent.com/s/tspl7jipr5nudpq/Nirvana-#{version}.zip"
